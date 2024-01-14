c9b5532525e57a50b4b79efca626d16f7f173a11
Frasia <108404961+SFrasia@users.noreply.github.com>
2024-01-14 09:00:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = cats.push("Ralph").length;
+
+const destructivelyPrependCat = cats.unshift("Bob");
+
+const destructivelyRemoveLastCat = cats.pop("Ralph");
+
+const destructivelyRemoveFirstCat = cats.shift("Ralph");
+
+const appendCat = cats.slice(3);
+
+const prependCat = cats.slice(0);
+
+const removeLastCat = cats.splice(0, -1);
+
+const removeFirstCat = cats.splice(0);
